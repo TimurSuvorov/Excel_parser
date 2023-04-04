02449f059f3843ac436229b7f105476d8ed69537
TSuvorov <suvorovt@gmail.com>
2023-04-04 23:05:51 +0300

    create model

--- ursip_site/ursip_parser/models.py
+++ ursip_site/ursip_parser/models.py
@@ -1,3 +1,15 @@
 from django.db import models
 
 # Create your models here.
+
+
+class Company(models.Model):
+    company = models.CharField(max_length=64)
+    fact_Qliq_data1 = models.IntegerField()
+    fact_Qliq_data2 = models.IntegerField()
+    fact_Qoil_data1 = models.IntegerField()
+    fact_Qoil_data2 = models.IntegerField()
+    forecast_Qliq_data1 = models.IntegerField()
+    forecast_Qliq_data2 = models.IntegerField()
+    forecast_Qoil_data1 = models.IntegerField()
+    forecast_Qoil_data2 = models.IntegerField()
